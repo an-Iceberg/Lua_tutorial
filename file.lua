dd0f26ac7276f0b06a2909139ebeb2168d4bc20a
an-Iceberg <a.g.marosi@bluewin.ch>
2022-10-16 17:46:00 +0200

    -

--- file.lua
+++ file.lua
@@ -1,9 +1,47 @@
-local file = io.open("myFile.txt", "a")
-
-print(file)
+local file = io.open("myFile.txt", "r")
 
 if file ~= nil then
-  file:write("\nHello World")
+  -- print(file)
+
+  --[[
+  for line in file:lines() do
+    print(line)
+  end
+  ]]
+
+  --[[
+  for i = 1, 11, 1 do
+    io.write(file:read(1).." ")
+  end
+  ]]
+
+  local letter
+  while true do
+    -- Reads 1 (the next) character from the file
+    letter = file:read(1)
+    if letter == nil then break end
+
+    io.write(letter.." ")
+  end
+
+  -- print(file:read("n"))
+
+  -- file:write("\nHello World")
+
+  file:close()
 end
 
-io.close(file)
+print()
+print()
+print("###")
+print()
+
+io.input("myFile.txt")
+
+local letter
+while true do
+  letter = io.read(1)
+  if letter == nil then break end
+
+  io.write(letter.." ")
+end
